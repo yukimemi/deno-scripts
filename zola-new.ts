9953f83b782ab2b863ae6ec88018fda00bcf8f92
yukimemi <yukimemi@gmail.com>
2021-04-03 11:26:20 +0900

    :zap: Fix import url to versioning.

--- zola-new.ts
+++ zola-new.ts
@@ -1,6 +1,7 @@
-import { formatISO } from "https://deno.land/x/date_fns/index.js";
-import * as color from "https://deno.land/std@0.88.0/fmt/colors.ts";
-import * as path from "https://deno.land/std/path/mod.ts";
+import * as color from "https://deno.land/std@0.92.0/fmt/colors.ts";
+import * as path from "https://deno.land/std@0.92.0/path/mod.ts";
+import { ensureDir } from "https://deno.land/std@0.92.0/fs/mod.ts";
+import { formatISO } from "https://deno.land/x/date_fns@v2.15.0/index.js";
 
 const input = async (): Promise<string> => {
   const title = await prompt("Enter new post title:");
@@ -17,6 +18,9 @@ const base = "./content";
 const today = formatISO(new Date(), { representation: "date" });
 const todayIso = formatISO(new Date(), {});
 const post = path.join(base, `${today}_${title}.md`);
+
+const parent = path.dirname(post);
+await ensureDir(parent);
 
 console.log(color.green(`Create new post: '${post}'`));
 
